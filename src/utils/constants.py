52a4120141651c7190a2d155e14c30f06605a582
Henry Rothenberg <hsr205@aol.com>
2026-08-24 14:42:30 -0400

    Add Queries class to constants.py module

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -3,3 +3,38 @@ class Constants:
     LOGGER_COLOR_WHITE: str = "\033[60m"
     LOGGER_COLOR_ORANGE: str = "\033[33m"
     LOGGER_COLOR_DARK_RED: str = "\033[31m"
+
+    POOL_MIN_CONNECTIONS: int = 1
+    POOL_MAX_CONNECTIONS: int = 5
+
+    class Queries:
+        ## ======================================================== CREATE TABLE QUERIES ======================================================== ##
+
+        CREATE_PLAYER_TABLE_SCHEMA_QUERY_STR: str = """
+            CREATE TABLE IF NOT EXISTS player (
+                id SERIAL PRIMARY KEY,
+                player_name VARCHAR(100) NOT NULL,
+                year_debuted INTEGER NOT NULL,
+                year_retired INTEGER NOT NULL,
+                hall_of_fame VARCHAR(1)
+            )
+        """
+
+        ## ======================================================== INSERT TABLE QUERIES ======================================================== ##
+
+        INSERT_INTO_PLAYER_TABLE_QUERY_STR: str = """
+                      INSERT INTO player (
+                            player_name,
+                            year_debuted,
+                            year_retired,
+                            hall_of_fame
+                      )
+                        VALUES (%s, %s, %s, %s);
+            """
+
+        ## ======================================================== QUERY TABLE QUERIES ======================================================== ##
+
+        QUERY_PLAYER_TABLE_FOR_ALL_MLB_PLAYERS: str = """
+            SELECT id, player_name
+            FROM player;
+        """
