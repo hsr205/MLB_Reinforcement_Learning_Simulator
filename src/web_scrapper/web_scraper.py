e13dc49eb4ba3d8979c9c8dba4ed5f2a6ddaff5b
Henry Rothenberg <hsr205@aol.com>
2026-08-24 16:18:38 -0400

    WIP web_scraper.py module

--- src/web_scrapper/web_scraper.py
+++ src/web_scrapper/web_scraper.py
@@ -2,12 +2,9 @@ import asyncio
 from logging import Logger
 
 from playwright.async_api import async_playwright, Page
-from src.web_scraper.data_cleanser import DataCleanser
-from tqdm import tqdm
 
 from src.config.config import Settings
 from src.logger.logger import AppLogger
-from src.utils.constants import Constants
 
 
 class WebScraper:
@@ -16,21 +13,6 @@ class WebScraper:
         self._base_url: str = settings.base_url
 
         self._logger: Logger = AppLogger.get_logger(self.__class__.__name__)
-
-    async def insert_rows_into_player_table(self) -> None:
-        mlb_players_list: list[tuple] = await self._web_scraper.get_all_mlb_players_list()
-
-        conn: connection = self._get_connection()
-        try:
-            self._logger.info(f"Inserting {len(mlb_players_list):,} rows in player table")
-            with conn.cursor() as cursor:
-                cursor.executemany(query=Constants.Queries.INSERT_INTO_PLAYER_TABLE_QUERY_STR,
-                                   vars_list=mlb_players_list)
-                conn.commit()
-            self._logger.info(f"Successfully inserted {len(mlb_players_list):,} rows in player table")
-        finally:
-            self._release_connection(conn)
-        self._logger.info("=" * 100)
 
     async def get_all_mlb_players_list(self) -> list[tuple]:
         all_mlb_players_tuple_list: list[tuple] = []
@@ -57,6 +39,35 @@ class WebScraper:
 
         return all_mlb_players_tuple_list
 
+    async def _extract_player_data(self, page: Page) -> list[tuple[str, int, int, str]]:
+
+        player_element_locator = await page.locator("#div_players_ > p").all()
+        player_tuples_list: list[tuple[str, int, int, str]] = []
+
+        for elem in player_element_locator:
+            full_text_str: str = await elem.text_content()
+
+            name_str: str = (await elem.locator("a").text_content()).strip()
+
+            career_years_str: str = full_text_str.replace(name_str, "").strip()
+
+            is_in_hall_of_fame_str: str = "Y" if "+" in career_years_str else "N"
+
+            years_partition_str: str = career_years_str.partition("(")[2].partition(")")[0]
+
+            start_year_str, end_year_str = years_partition_str.split("-")
+
+            year_debuted_int: int = int(start_year_str)
+            year_retired_int: int = int(end_year_str)
+
+            player_tuples_list.append(
+                (name_str, year_debuted_int, year_retired_int, is_in_hall_of_fame_str)
+            )
+
+        self._logger.info(f"Scraped {len(player_tuples_list):,} player rows")
+
+        return player_tuples_list
+
     async def navigate_to_base_url(self, page: Page) -> None:
         self._logger.info(f"Navigating to {self._base_url}")
         await page.goto(url=self._base_url)
@@ -70,54 +81,6 @@ class WebScraper:
         await asyncio.sleep(1)
         self._logger.info(
             f"Clicked on Players with last names starting with: '{first_letter_of_last_name_str.upper()}'")
-
-    async def _extract_player_data(self, page: Page) -> list[tuple[str, int, int, str]]:
-
-        player_element_locator: Locator = await page.locator("#div_players_ > p").all()
-        player_tuples_list: list[tuple[str, int, int, str]] = []
-
-        for elem in player_element_locator:
-            full_text_str: str = await elem.text_content()
-
-            name_str: str = (await elem.locator("a").text_content()).strip()
-
-            career_years_str: str = full_text_str.replace(name_str, "").strip()
-
-            is_in_hall_of_fame_str: str = "Y" if "+" in career_years_str else "N"
-
-            # Isolate years string between '(' and ')' -> "1954-1976"
-            years_part = career_years_str.partition("(")[2].partition(")")[0]
-
-            start_year_str, end_year_str = years_part.split("-")
-
-            year_debuted_int: int = int(start_year_str)
-            year_retired_int: int = int(end_year_str)
-
-            player_tuples_list.append(
-                (name_str, year_debuted_int, year_retired_int, is_in_hall_of_fame_str)
-            )
-
-        self._logger.info(f"Scraped {len(players_list)} player rows")
-
-        return player_tuples
-
-        # table_locator: Locator = page.locator("table#players")
-        #
-        # await table_locator.wait_for()
-        #
-        # table_rows_list: list[Locator] = await table_locator.locator("tbody tr:not(.thead)").all()
-        #
-        # players_list: list[tuple] = []
-        # for row in table_rows_list:
-        #     cells: list[str] = await row.locator("th, td").all_inner_texts()
-        #     cells[0] = cells[0].replace("*", "")
-        #     sanitized_player_tuple: tuple = self._data_cleanser.sanitize_player_row(cells=cells)
-        #     players_list.append(sanitized_player_tuple)
-        #
-        # self._logger.info(f"Scraped {len(players_list)} player rows")
-        # self._logger.info("=" * 100)
-        #
-        # return players_list
 
     def _get_alphabet_list(self) -> list[str]:
 
