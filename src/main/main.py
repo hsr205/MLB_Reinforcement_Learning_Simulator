48b830dfce513ad107e02966af8716a75c989cf6
Henry Rothenberg <hsr205@aol.com>
2026-08-24 16:19:13 -0400

    Alter demo logic in main.py module

--- src/main/main.py
+++ src/main/main.py
@@ -1,21 +1,20 @@
+import asyncio
 from logging import Logger
 
-from src.web_scraper.web_scrapper import WebScraper
-
 from src.config.config import Settings
+from src.database.database_client import DatabaseClient
 from src.logger.logger import AppLogger
 
 
-def main() -> int:
+async def main() -> int:
     logger: Logger = AppLogger().get_logger(class_name=str(__name__))
     settings: Settings = Settings()
 
-    web_scraper: WebScraper = WebScraper(settings=settings)
+    database_client: DatabaseClient = DatabaseClient(settings=settings)
 
     try:
-        # logger.info(f"Inside main() method")
 
-        database_client.create_player_table()
+        await database_client.insert_rows_into_player_table()
 
         return 0
     except Exception as e:
@@ -25,4 +24,4 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
