637f447adfe9fed0f0bd084a33a78035d8d638d1
Henry Rothenberg <hsr205@aol.com>
2026-08-25 09:38:29 -0400

    Add logic to data_processor.py module

--- src/data_processor/data_processor.py
+++ src/data_processor/data_processor.py
@@ -1,9 +1,23 @@
+from datetime import datetime
 from logging import Logger
+
+from pandas import DataFrame
+from pybaseball import playerid_lookup, statcast_batter
 
 from logger.logger import AppLogger
 
-# TODO: Leverage the PyBaseball library to extract relevant stat data
+
 class DataProcessor:
 
     def __init__(self) -> None:
+        self._start_datetime_str: str = datetime(year=2026, month=3, day=25).strftime('%Y-%m-%d')
+        self._current_datetime_str: str = datetime.today().strftime('%Y-%m-%d')
         self._logger: Logger = AppLogger.get_logger(self.__class__.__name__)
+
+    def display_sample_statcast_data_retrieval(self) -> None:
+        player_id_int: int = playerid_lookup(last='judge', first='aaron')['key_mlbam'][0]
+
+        sample_stats_dataframe: DataFrame = statcast_batter(start_dt=self._start_datetime_str,
+                                                            end_dt=self._current_datetime_str, player_id=player_id_int)
+
+        self._logger.info(f"sample_stats_dataframe.columns = {sample_stats_dataframe.columns}")
